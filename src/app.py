35bccd8a8a8e38f1a1d57a3603f23cd8db4176b3
Shobhit Gupta <shobhit.gupta@codenation.co.in>
2020-12-11 18:54:38 +0000

    Sample Weather app on devspaces done

--- src/app.py
+++ src/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
-
+from flask import Flask,render_template,request
+import requests,json
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "<h3><b>This is a sample hello world file for Devspaces</b></h3>"
+def home():
+    return render_template('home.html')
+ 
+@app.route('/get_weather',methods=['POST'])
+def get_weather():
+    city=request.form['city']
+    print("City is : ",city)
+    url="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=13f855955e1f0cd65acd02eb09eedfa5"
+    response=json.loads(requests.get(url).text)
+    data=dict()
+    data['city']=city
+    if response['cod']!="404":
+        data['status']=200
+        data['weather']=response['weather'][0]['main']+" - "+response['weather'][0]['description']
+        data['temp']=response['main']['temp']-273.15
+    else:
+        data['status']=404
+    return render_template('home.html',data=data)
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
