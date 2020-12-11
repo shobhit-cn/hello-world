d17f1b3d60b55aa2230ca6767f0ccc771127ae72
Shobhit Gupta <shobhit.gupta@codenation.co.in>
2020-12-11 08:31:30 +0000

    Sample app on devspaces done

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "<h3><b>This is a sample hello world file for Devspaces</b></h3>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
